5aa37509c1d9d8e93abe32cf1debca8cca0dec23
Poukiladybug <elisevan_poucke@outlook.com>
2021-11-12 17:20:07 +0100

    changes 3rd game

--- Assets/ScriptBoulanger/FilltheBecher.cs
+++ Assets/ScriptBoulanger/FilltheBecher.cs
@@ -12,6 +12,17 @@ public class FilltheBecher : MonoBehaviour, IPointerClickHandler
     public Image becher;
     private bool playerClick = false;
     private int clik = 1;
+    public static float levelOfLevure;
+    public static float levelOfFarine;
+    public static float levelOfEau;
+    public static float levelOfBeure;
+    public static float levelOfSel;
+    public static bool sel = false;
+    public static bool farine = false;
+    public static bool levure = false;
+    public static bool eau = false;
+    public static bool beure = false;
+    
     //float filled = becher.fillAmount;
 
     // Start is called before the first frame update
@@ -20,17 +31,53 @@ public class FilltheBecher : MonoBehaviour, IPointerClickHandler
         becher = GetComponent<Image>();
         obj.SetActive(true);
         becher.fillAmount = 0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Input.GetMouseButtonDown (pour faire fonctionner le fill)?
+        
         if (playerClick)
         {
             becher.fillAmount += Time.deltaTime / 10;
-             
+
+            if (obj.CompareTag("farine"))
+            {
+                levelOfFarine = becher.fillAmount;
+                farine = true;
+                Debug.Log("farine: " + farine);
+            }
+            else if (obj.CompareTag("levure"))
+            {
+                levelOfLevure = becher.fillAmount;
+                levure = true;
+                Debug.Log("levure: " + levure);
+            }
+            else if (obj.CompareTag("eau"))
+            {
+                levelOfEau = becher.fillAmount;
+                eau = true;
+                Debug.Log("eau: " + eau);
+            }
+            else if (obj.CompareTag("beure"))
+            {
+                levelOfBeure = becher.fillAmount;
+                beure = true;
+                Debug.Log("beure: " + beure);
+            }
+            else if (obj.CompareTag("sel"))
+            {
+                levelOfSel = becher.fillAmount;
+                sel = true;
+                Debug.Log("sel: " + sel);
+            }
         }
+
+            
+            //Input.GetMouseButtonDown (pour faire fonctionner le fill)?
+        
+       
     }
 
     public void OnPointerClick(PointerEventData eventData)
