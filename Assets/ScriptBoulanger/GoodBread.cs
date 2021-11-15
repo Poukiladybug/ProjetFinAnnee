9adf214c8a741c42f30c69e11fbe352def256ea6
Poukiladybug <elisevan_poucke@outlook.com>
2021-11-15 15:01:10 +0100

    bread

--- Assets/ScriptBoulanger/GoodBread.cs
+++ Assets/ScriptBoulanger/GoodBread.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class GoodBread : MonoBehaviour
 {
-    private float levelOfFarine;
+    public GameObject goodBread;
+    public GameObject badBread;
+    public float levelOfFarine;
     private float levelOfLevure;
     private float levelOfBeure;
     private float levelOfEau;
     private float levelOfSel;
+    private bool oneBread = true;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (BoulangerSceneChanger.isPressed == true)
         {
@@ -26,7 +28,29 @@ public class GoodBread : MonoBehaviour
             levelOfEau = FilltheBecher.eau;
             levelOfSel = FilltheBecher.sel;
         }
+
+        if (oneBread)
+        {
+            createBread();
+            oneBread = false;
+        }
+
+    }
+
+    public void createBread()
+    {
         
 
+        Vector3 position = new Vector3(0f, 0f, 0f);
+
+        if( levelOfFarine > 0.7f && levelOfFarine < 0.8f && levelOfLevure >0.4f && levelOfLevure < 0.5f && levelOfBeure > 0.1f && levelOfBeure < 0.2f && levelOfEau > 0.1f && levelOfEau < 0.2f && levelOfSel < 0.1f)
+        {
+            GameObject item = Instantiate(goodBread, position, Quaternion.identity);
+        }
+
+        else
+        {
+            GameObject item = Instantiate(badBread, position, Quaternion.identity);
+        }
     }
 }
--- Assets/ScriptBoulanger/SetFireToTheBread.cs
+++ Assets/ScriptBoulanger/SetFireToTheBread.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SetFireToTheBread : MonoBehaviour
 {
+    public float heat;
+    public Image feu;
+    private bool islighten = false;
 
     
 
     // Update is called once per frame
     public void SetFire(Image image)
     {
-        image.fillAmount += 0.02f;
+        image.fillAmount += heat;
+        islighten = true;
+        if (image.fillAmount >= 1f)
+        {
+            Change("Bread");
+        }
     }
-}
+
+    private void Update()
+    {
+        if (islighten)
+        {
+            feu.fillAmount -= 0.001f;
+
+            //if (feu.fillAmount < 0f)
+            //{
+            //    islighten = false;
+            //}
+        }
+        
+    }
+
+    public void Change(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    }
