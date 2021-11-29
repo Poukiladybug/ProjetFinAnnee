19153b81528e52298f471fa4968c666e097bc617
Poukiladybug <elisevan_poucke@outlook.com>
2021-11-29 11:45:22 +0100

    first commit home

--- Assets/playerActions.cs
+++ Assets/playerActions.cs
@@ -108,6 +108,33 @@ public class @PlayerActions : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""test"",
+            ""id"": ""b88f7f64-1a12-4149-9a27-e51c27f1face"",
+            ""actions"": [
+                {
+                    ""name"": ""pelle"",
+                    ""type"": ""Value"",
+                    ""id"": ""81814e5c-c7c5-4873-b561-e6b606a46e74"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""002a641e-5d38-4554-90a4-fbec99a0ced8"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""pelle"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -119,6 +146,9 @@ public class @PlayerActions : IInputActionCollection, IDisposable
         m_ArcherGameplay = asset.FindActionMap("ArcherGameplay", throwIfNotFound: true);
         m_ArcherGameplay_Shoot = m_ArcherGameplay.FindAction("Shoot", throwIfNotFound: true);
         m_ArcherGameplay_Movement = m_ArcherGameplay.FindAction("Movement", throwIfNotFound: true);
+        // test
+        m_test = asset.FindActionMap("test", throwIfNotFound: true);
+        m_test_pelle = m_test.FindAction("pelle", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -238,6 +268,39 @@ public class @PlayerActions : IInputActionCollection, IDisposable
         }
     }
     public ArcherGameplayActions @ArcherGameplay => new ArcherGameplayActions(this);
+
+    // test
+    private readonly InputActionMap m_test;
+    private ITestActions m_TestActionsCallbackInterface;
+    private readonly InputAction m_test_pelle;
+    public struct TestActions
+    {
+        private @PlayerActions m_Wrapper;
+        public TestActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @pelle => m_Wrapper.m_test_pelle;
+        public InputActionMap Get() { return m_Wrapper.m_test; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(TestActions set) { return set.Get(); }
+        public void SetCallbacks(ITestActions instance)
+        {
+            if (m_Wrapper.m_TestActionsCallbackInterface != null)
+            {
+                @pelle.started -= m_Wrapper.m_TestActionsCallbackInterface.OnPelle;
+                @pelle.performed -= m_Wrapper.m_TestActionsCallbackInterface.OnPelle;
+                @pelle.canceled -= m_Wrapper.m_TestActionsCallbackInterface.OnPelle;
+            }
+            m_Wrapper.m_TestActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @pelle.started += instance.OnPelle;
+                @pelle.performed += instance.OnPelle;
+                @pelle.canceled += instance.OnPelle;
+            }
+        }
+    }
+    public TestActions @test => new TestActions(this);
     public interface IFireGameplayActions
     {
         void OnMovement(InputAction.CallbackContext context);
@@ -246,5 +309,9 @@ public class @PlayerActions : IInputActionCollection, IDisposable
     {
         void OnShoot(InputAction.CallbackContext context);
         void OnMovement(InputAction.CallbackContext context);
+    }
+    public interface ITestActions
+    {
+        void OnPelle(InputAction.CallbackContext context);
     }
 }
